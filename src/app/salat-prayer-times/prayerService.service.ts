2f409828cd1e63e104ef201515a67cd7201c2070
Amirouche <bessaa.amirouche@outlook.fr>
2024-03-11 12:41:54 +0100

    heure systèmes corrigée

--- src/app/salat-prayer-times/prayerService.service.ts
+++ src/app/salat-prayer-times/prayerService.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Observable, timer } from "rxjs";
-import { switchMap, map } from "rxjs/operators";
+import { Observable } from "rxjs";
+import { map } from "rxjs/operators";
 import { Prayer } from "./prayer.interface";
 
 @Injectable({
@@ -12,22 +12,9 @@ export class PrayerService {
   private url2 = 'http://86.48.1.128:7000/infoJour';
 
   constructor(private http: HttpClient) {
-    this.startInterval();
+    
   }
 
-  startInterval() {
-    // Lancez une première fois dès le début
-    this.getPrayerTime().subscribe();
-    this.getActualTime().subscribe();
-
-    // Ensuite, exécutez toutes les 30 minutes
-    timer(30 * 60 * 1000, 30 * 60 * 1000) // Démarre après 30 minutes et se répète toutes les 30 minutes
-      .pipe(
-        switchMap(() => this.getPrayerTime()),
-        switchMap(() => this.getActualTime())
-      )
-      .subscribe();
-  }
 
   getPrayerTime(): Observable<Prayer> {
     return this.http.get<Prayer>(this.url).pipe(
